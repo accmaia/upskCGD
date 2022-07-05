d2a7cdfd35df11bc544bf55c2c7e621fa4020594
aclemenm <andre.clemente.maia@hotmail.com>
2022-07-05 13:48:40 +0100

    Initial commit

---
+++ soaprest/src/main/java/com/nttdata/formacao/soaprest/SoaprestApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.formacao.soaprest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaprestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoaprestApplication.class, args);
+	}
+
+}
